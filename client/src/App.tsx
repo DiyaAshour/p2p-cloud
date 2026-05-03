458323e00d7522e6f12b8ced1befc8b82a1afc52
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-05-04 02:06:00 +0300

    Use single dashboard entrypoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,7 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/index.html" component={Home} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/404" component={NotFound} />
-      <Route component={Home} />
-    </Switch>
-  );
-}
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  return <Dashboard />;
 }
 
 export default App;
