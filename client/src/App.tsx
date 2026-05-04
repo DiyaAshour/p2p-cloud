857062a28206ef721924cc4b19c3bdce215b28b7
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-05-05 00:12:31 +0300

    Enable Drive Mode UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import NativeP2PApp from "./NativeP2PApp";
+import DriveP2PApp from "./DriveP2PApp";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <NativeP2PApp />
+          <DriveP2PApp />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
