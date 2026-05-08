070a20a8aeaca53dc5e8b4c856a68d3609290c34
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-05-09 01:49:28 +0300

    Use live app with downloadToPath only

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import NativeP2PAppStable from "./NativeP2PAppStable";
+import NativeP2PAppLive from "./NativeP2PAppLive";
 import TransferProgressOverlay from "./TransferProgressOverlay";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <ThemeProvider defaultTheme="dark">
         <TooltipProvider>
           <Toaster />
-          <NativeP2PAppStable />
+          <NativeP2PAppLive />
           <TransferProgressOverlay />
         </TooltipProvider>
       </ThemeProvider>
