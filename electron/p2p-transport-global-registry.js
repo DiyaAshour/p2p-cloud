29f1dba3dd5a52016a4a2952bfa136bb16eed9c0
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-05-15 08:19:16 +0300

    p2p: expose transport node globally for company object replication

---
+++ electron/p2p-transport-global-registry.js
@@ -0,0 +1,21 @@
+import { P2PTransportNode } from './p2p-transport.js';
+
+if (!P2PTransportNode.prototype.__chunknetGlobalRegistryPatched) {
+  const originalStart = P2PTransportNode.prototype.start;
+
+  P2PTransportNode.prototype.start = function patchedStart(...args) {
+    const result = originalStart.apply(this, args);
+    globalThis.__p2pTransportNode = this;
+    globalThis.__p2pNode = this;
+    console.log('[p2p-transport] exposed global transport node for distributed company objects');
+    return result;
+  };
+
+  Object.defineProperty(P2PTransportNode.prototype, '__chunknetGlobalRegistryPatched', {
+    value: true,
+    enumerable: false,
+    configurable: false,
+  });
+
+  console.log('[p2p-transport] global registry patch installed');
+}
