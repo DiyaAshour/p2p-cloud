032a74f2b2d4a5a176280dd329daa6d5954afced
DiyaAshour <165673884+DiyaAshour@users.noreply.github.com>
2026-05-25 17:40:02 +0300

    feat: import folder CRUD IPC module

--- electron/main-wrapper.js
+++ electron/main-wrapper.js
@@ -267,6 +267,8 @@ async function importMainWhenReady() {
     console.log('[main-wrapper] file update IPC import finished');
     await import('./folder-item-ipc.js');
     console.log('[main-wrapper] folder item IPC import finished');
+    await import('./folder-crud-ipc.js');
+    console.log('[main-wrapper] folder CRUD IPC import finished');
     await import('./ui-prefs-ipc.js');
     console.log('[main-wrapper] UI preferences IPC import finished');
     await import('./protected-upload-override.js');
